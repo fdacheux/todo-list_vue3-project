6dcea8eb8442ae1dd32dcc7d752ccd2acd80c170
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-14 15:56:50 +0100

    feat(all): initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import Header from "./components/Header/HeaderItem.vue";
+  import Home from "./views/HomeView.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <Header />
+    <Home />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>La To-Do Liste</h1>
+  </header>
+</template>
+
+<style scoped></style>
