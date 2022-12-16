10171d4b83706edf6ab6030ad6cac69505c304c2
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-16 16:49:49 +0100

    feat(results): create + style results component

---
+++ src/components/TodoList/Results/ResultsItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="cards-group">
+    <div class="card">
+      <p>This is an example</p>
+      <button class="btn btn-danger">X</button>
+    </div>
+  </div>
+</template>
+
+<style scoped src="./ResultsItem.css"></style>
--- src/components/TodoList/TodoListItem.vue
+++ src/components/TodoList/TodoListItem.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import Form from "./Form/FormItem.vue";
+  import Results from "./Results/ResultsItem.vue";
 </script>
 
 <template>
   <div class="container">
     <h2 class="title display-5 text-center">Please, enter tasks</h2>
     <Form />
+    <Results class="mt-4" />
   </div>
 </template>
 
