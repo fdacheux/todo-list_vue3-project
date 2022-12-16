3a8b06a670025351c0743e44fe37442c9e3ccd0d
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-16 23:01:35 +0100

    feat(todo list): add/removal tasks management

--- src/components/TodoList/Form/FormItem.vue
+++ src/components/TodoList/Form/FormItem.vue
@@ -1,13 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { reactive, ref } from "vue";
+  import ResultItem from "./ResultItem.vue";
+  const formData = ref({
+    task: "",
+  });
+  const resultsArr: string[] = reactive([]);
+  const addTask = (task: string) => {
+    resultsArr.push(task);
+    formData.value.task = "";
+  };
+  const deleteTask = (index: number) => {
+    resultsArr.splice(index, 1);
+  };
+</script>
 
 <template>
-  <form>
-    <div class="form-group">
-      <label for="todo" class="mt-4 mb-2">Task </label>
-      <input type="text" id="todo" class="form-control" />
-    </div>
-    <button class="btn btn-primary mt-4">Create task</button>
-  </form>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="todo" class="mt-4 mb-2">Task </label>
+        <input
+          type="text"
+          id="todo"
+          class="form-control"
+          v-model="formData.task"
+        />
+      </div>
+      <button
+        class="btn btn-primary mt-4"
+        @click.prevent="addTask(formData.task)"
+      >
+        Create task
+      </button>
+    </form>
+    <ul class="cards-group mt-4">
+      <li class="card mt-2" :key="index" v-for="(task, index) in resultsArr">
+        <ResultItem @removeTask="deleteTask" :id="index" :task="task" />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped src="./FormItem.css"></style>
---
+++ src/components/TodoList/Form/ResultItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { numberLiteralTypeAnnotation } from "@babel/types";
+  import { defineProps } from "vue";
+
+  const props = defineProps({
+    task: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <p>{{ props.task }}</p>
+  <button
+    class="btn btn-danger btn-delete"
+    @click="$emit('removeTask', props.id)"
+  >
+    X
+  </button>
+</template>
+
+<style scoped src="./FormItem.css"></style>
